f00e2dc38fe136cdc33eda134a9244bce9705b78
Emerson <emermoraes03@gmail.com>
2024-06-24 22:26:22 -0300

    Inicio do projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Home from './components/Home'
+
+function App() {
+
+
+  return (
+   <div className='project'>
+    <Home />
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,13 @@
+import './Home.css'
+
+let Home = () => {
+
+ return (
+    <div>
+    <h1>aaaaa</h1>
+    </div>
+ )
+
+}
+
+export default Home
