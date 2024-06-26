172293ae86ec268a763cd9c25e82e4d393cc6c65
Emerson <emermoraes03@gmail.com>
2024-06-25 22:13:12 -0300

    Finalização da homepage, faltando apenas animações

---
+++ src/components/Home/Header/index.jsx
@@ -0,0 +1,27 @@
+import './Header.css'
+import logo from '../../../../public/images/logo-black.png'
+
+let Header = () => {
+    return(
+        <header className='header'>
+            <div className='header-logo-container'>
+                <img src={logo} alt='logo'/>
+            </div>
+            <div className='nav-container'>
+                <ul className='nav'>
+                    <li><a>Início</a></li>
+                    <li><a>Sobre nós</a></li>
+                    <li><a>Soluções</a></li>
+                    <li><a>Parceiros</a></li>
+                </ul>
+            </div>
+            <div className='header-button-container'>
+                <button className='header-button'><a>Contato</a></button>
+            </div>
+            
+        </header>
+
+    )
+}
+
+export default Header
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,11 +1,32 @@
+import Header from './Header'
 import './Home.css'
+import facebook from '../../../public/images/facebook-white-icon.svg'
+import instagram from '../../../public/images/instagram-white-icon.svg'
+import email from '../../../public/images/email-white-icon.svg'
 
 let Home = () => {
 
  return (
-    <div>
-    <h1>aaaaa</h1>
-    </div>
+    <section className='home'>
+      <Header  />
+      <div className='home-itens'>
+         <div className='home-itens-container'>
+         <div className='home-itens-title'>
+            <h1 className='home-itens-title-text'>Transforme seu produto ou serviços em oportunidades reais através do <strong>processo de licitações.</strong></h1>
+         </div>
+         <div className='home-itens-subtitle'>
+            <h3>Expertise em licitações, ao seu alcance.</h3>
+         </div>
+         <div className='home-itens-logos'>
+            <a><img src={facebook}/></a>
+            <a><img src={instagram}/></a>
+            <a><img src={email}/></a>
+         </div>
+         <div className='home-itens-button'><button><a>Fale conosco!</a></button></div>
+
+         </div>
+      </div>
+    </section>
  )
 
 }
