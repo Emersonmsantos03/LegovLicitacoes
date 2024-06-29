b18a18da915cbe87a7ca27beba45f9087508e6d1
Emerson <emermoraes03@gmail.com>
2024-06-29 12:00:02 -0300

    Alterando e estilizando melhor o que já foi feito até agora

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import About from './components/About'
 import Home from './components/Home'
+import Soluctions from './components/Soluctions'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
    <div className='project'>
     <Home />
     <About  />
+    <Soluctions />
    </div>
   )
 }
---
+++ src/components/Soluctions/index.jsx
@@ -0,0 +1,20 @@
+import './Soluctions.css'
+
+let Soluctions = () =>{
+    return(
+
+        <section className='soluctions'> 
+            <div className='soluctions-container'>
+                <div className='ojectives'>
+                    <div className='objetives-title'></div>
+                    <div className='objetives-cards'></div>
+                </div>
+                <div className='soluctions'></div>
+
+            </div>
+             </section>
+    )
+
+}
+
+export default Soluctions
