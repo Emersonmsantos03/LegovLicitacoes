eb62b153aa77bff8d4082489f7d0e6bfc25739bd
Emerson <emermoraes03@gmail.com>
2024-07-30 20:12:18 -0300

    finalizando site

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,8 +1,8 @@
 import Header from './Header'
 import './Home.css'
-import facebook from '../../../public/images/facebook-white-icon.svg'
-import instagram from '../../../public/images/instagram-white-icon.svg'
-import email from '../../../public/images/email-white-icon.svg'
+import map from '../../../public/images/map.png'
+import instagram from '../../../public/images/instagram.png'
+import email from '../../../public/images/email.png'
 import { motion } from 'framer-motion';
 import React, { useState, useEffect } from 'react';
 
@@ -30,9 +30,9 @@ let Home = () => {
             <h3>Expertise em licitações, ao seu alcance.</h3>
          </div>
          <div className='home-itens-logos'>
-            <a><img src={facebook}/></a>
-            <a><img src={instagram}/></a>
-            <a><img src={email}/></a>
+            <a href='https://www.google.com/maps/place/R.+Ant%C3%B4nio+Dalmarco,+495+-+Fazendinha,+Curitiba+-+PR,+81320-420/@-25.4791763,-49.3221035,15z/data=!4m16!1m9!3m8!1s0x94dce31cff6f8cab:0xc2fcc5c031ca3025!2sR.+Ant%C3%B4nio+Dalmarco,+495+-+Fazendinha,+Curitiba+-+PR,+81320-420!3b1!8m2!3d-25.4793313!4d-49.3219888!10e5!16s%2Fg%2F11csc5ny_d!3m5!1s0x94dce31cff6f8cab:0xc2fcc5c031ca3025!8m2!3d-25.4793313!4d-49.3219888!16s%2Fg%2F11csc5ny_d?entry=ttu' target="_blank"><img src={map}/></a>
+            <a href='https://www.instagram.com/legov.licita/' target="_blank"><img src={instagram}/></a>
+            <a href='mailto:legov.licita@gmail.com' target="_blank"><img src={email}/></a>
          </div>
          <div className='home-itens-button'><button><a href="https://wa.me/5541995043150?text=Ol%C3%A1%21%20Gostaria%20de%20saber%20mais%20sobre%20como%20aumentar%20as%20minhas%20chances%20no%20processo%20de%20licita%C3%A7%C3%A3o%21" target="_blank">Fale conosco!</a></button></div>
 
